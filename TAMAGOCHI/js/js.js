d466e3861acbc52f7f72ff3ff90b27c0612d4d78
Yaroslav-Teplyi <57116311+Yaroslav-Teplyi@users.noreply.github.com>
2019-12-18 15:52:46 +0300

    without difficulty and game over

---
+++ TAMAGOCHI/js/js.js
@@ -0,0 +1,118 @@
+'use strict';
+
+let foodPg = document.getElementById('foodProg'),
+    cleanPg = document.getElementById('cleanProg'),
+    happinessPg = document.getElementById('happinessProg'),
+    countFood = Math.ceil(Math.random() * (100 - 50) + 50),
+    countClean = Math.ceil(Math.random() * (100 - 50) + 50),
+    countHappiness = Math.ceil(Math.random() * (100 - 50) + 50),
+    countSec = 0;
+    // var naz = true
+
+foodPg.value = countFood;
+cleanPg.value = countClean;
+happinessPg.value = countHappiness;
+
+document.getElementById('easyLevelBtn').onclick = function() {
+    start();
+    setInterval(easyDiff, 5000);
+    setInterval(duringLife, 1000);
+    setInterval(gameOver, 100);
+}
+
+document.getElementById('hardLevelBtn').onclick = function() {
+    start();
+    setInterval(hardDiff, 5000);
+    setInterval(duringLife, 1000);
+}
+
+function start() {
+    document.querySelector('.start').hidden = true;
+
+    let actions = document.querySelector('.actions'),
+        style = actions.style;
+    actions.classList.add('actions-on');
+}
+
+document.getElementById('eatBtn').onclick = function() {
+    if(countFood <= 70 && countFood > 0){ 
+    countFood += 30;
+    countClean -= 20;
+
+    foodPg.value = countFood;
+    cleanPg.value = countClean;
+    } else if(countFood > 70 && countFood <= 100) {
+        countFood = 100
+        countClean -= 20;
+
+        foodPg.value = countFood;
+        cleanPg.value = countClean;
+    }
+}
+
+document.getElementById('washBtn').onclick = function() {
+    if(countClean <= 60 && countClean > 0){ 
+    countClean += 40;
+    countHappiness -= 20;
+
+    happinessPg.value = countHappiness;
+    cleanPg.value = countClean;
+    } else if(countClean > 60 && countClean <= 100) {
+        countClean = 100
+        countHappiness -= 20;
+
+        happinessPg.value = countHappiness;
+        cleanPg.value = countClean;
+    }
+}
+
+document.getElementById('runBtn').onclick = function() {
+    if(countHappiness <= 85 && countHappiness > 0){ 
+
+    countHappiness += 15;
+    countFood -= 10;
+
+    happinessPg.value = countHappiness;
+    foodPg.value = countFood;
+    } else if(countHappiness > 85 && countHappiness <= 100) {
+        countHappiness = 100
+        countFood -= 20;
+
+        happinessPg.value = countHappiness;
+        foodPg.value = countFood;
+    }
+}
+
+function easyDiff() {
+    countFood -= 3;
+    countClean -= 3;
+    countHappiness -= 3;
+
+    foodPg.value = countFood;
+    happinessPg.value = countHappiness;
+    cleanPg.value = countClean;
+}
+
+function hardDiff() {
+    countFood -= 5;
+    countClean -= 5;
+    countHappiness -= 5;
+
+    foodPg.value = countFood;
+    happinessPg.value = countHappiness;
+    cleanPg.value = countClean;
+}
+
+function duringLife() {
+    countSec += 1;
+    console.log(countSec);
+}
+// function gameOver(){
+//     if(naz){
+//     if(foodPg.value <= 0 || cleanPg.value <= 0 || happinessPg.value <= 0) {
+//         alert(1)
+//         document.querySelector('.actions').hidden = true;
+//         naz = false;
+//     }
+// }
+// }
