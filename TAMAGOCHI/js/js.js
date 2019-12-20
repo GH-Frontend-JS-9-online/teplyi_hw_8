21399f1bacadac8a81495d28f477cabff128bcc9
Yaroslav-Teplyi <57116311+Yaroslav-Teplyi@users.noreply.github.com>
2019-12-20 16:41:26 +0300

    Add files via upload

--- TAMAGOCHI/js/js.js
+++ TAMAGOCHI/js/js.js
@@ -6,8 +6,9 @@ let foodPg = document.getElementById('foodProg'),
     countFood = Math.ceil(Math.random() * (100 - 50) + 50),
     countClean = Math.ceil(Math.random() * (100 - 50) + 50),
     countHappiness = Math.ceil(Math.random() * (100 - 50) + 50),
-    countSec = 0;
-    // var naz = true
+    countSec = 0,
+    isGameOver = true,
+    isGameScore = true;
 
 foodPg.value = countFood;
 cleanPg.value = countClean;
@@ -24,6 +25,7 @@ document.getElementById('hardLevelBtn').onclick = function() {
     start();
     setInterval(hardDiff, 5000);
     setInterval(duringLife, 1000);
+    setInterval(gameOver, 100);
 }
 
 function start() {
@@ -104,15 +106,21 @@ function hardDiff() {
 }
 
 function duringLife() {
+    if(isGameScore){
     countSec += 1;
-    console.log(countSec);
+
+    if(foodPg.value <= 0 || cleanPg.value <= 0 || happinessPg.value <= 0) {
+        alert('You have been lived for ' + countSec + ' seconds')
+        isGameScore = false;
+    }
+}}
+function gameOver(){
+    if(isGameOver){
+    if(foodPg.value <= 0 || cleanPg.value <= 0 || happinessPg.value <= 0) {
+        document.querySelector('.actions').hidden = true;
+        isGameOver = false;
+        clearInterval(hardDiff, 100);
+        clearInterval(duringLife, 100);
+    }
 }
-// function gameOver(){
-//     if(naz){
-//     if(foodPg.value <= 0 || cleanPg.value <= 0 || happinessPg.value <= 0) {
-//         alert(1)
-//         document.querySelector('.actions').hidden = true;
-//         naz = false;
-//     }
-// }
-// }
+}
