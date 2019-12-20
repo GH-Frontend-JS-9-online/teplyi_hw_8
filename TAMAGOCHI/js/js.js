809d40fd9265bd35e8e0723fff9b7261985fbd77
Yaroslav-Teplyi <57116311+Yaroslav-Teplyi@users.noreply.github.com>
2019-12-20 17:02:44 +0300

    Add files via upload

--- TAMAGOCHI/js/js.js
+++ TAMAGOCHI/js/js.js
@@ -3,16 +3,9 @@
 let foodPg = document.getElementById('foodProg'),
     cleanPg = document.getElementById('cleanProg'),
     happinessPg = document.getElementById('happinessProg'),
-    countFood = Math.ceil(Math.random() * (100 - 50) + 50),
-    countClean = Math.ceil(Math.random() * (100 - 50) + 50),
-    countHappiness = Math.ceil(Math.random() * (100 - 50) + 50),
     countSec = 0,
     isGameOver = true,
     isGameScore = true;
-
-foodPg.value = countFood;
-cleanPg.value = countClean;
-happinessPg.value = countHappiness;
 
 document.getElementById('easyLevelBtn').onclick = function() {
     start();
@@ -32,58 +25,72 @@ function start() {
     document.querySelector('.start').hidden = true;
 
     let actions = document.querySelector('.actions'),
-        style = actions.style;
+        style = actions.style,
+        countFood = Math.ceil(Math.random() * (100 - 50) + 50),
+        countClean = Math.ceil(Math.random() * (100 - 50) + 50),
+        countHappiness = Math.ceil(Math.random() * (100 - 50) + 50),
+        countSec = 0,
+        isGameOver = true,
+        isGameScore = true;
+
+    foodPg.value = countFood;
+    cleanPg.value = countClean;
+    happinessPg.value = countHappiness;
     actions.classList.add('actions-on');
-}
-
-document.getElementById('eatBtn').onclick = function() {
-    if(countFood <= 70 && countFood > 0){ 
-    countFood += 30;
-    countClean -= 20;
-
-    foodPg.value = countFood;
-    cleanPg.value = countClean;
-    } else if(countFood > 70 && countFood <= 100) {
-        countFood = 100
+    if(isGameOver == false) {
+        actions.classList.remove('actions-on');
+    }
+    document.getElementById('eatBtn').onclick = function() {
+        if(countFood <= 70 && countFood > 0){ 
+        countFood += 30;
         countClean -= 20;
-
+    
         foodPg.value = countFood;
         cleanPg.value = countClean;
+        } else if(countFood > 70 && countFood <= 100) {
+            countFood = 100
+            countClean -= 20;
+    
+            foodPg.value = countFood;
+            cleanPg.value = countClean;
+        }
     }
-}
-
-document.getElementById('washBtn').onclick = function() {
-    if(countClean <= 60 && countClean > 0){ 
-    countClean += 40;
-    countHappiness -= 20;
-
-    happinessPg.value = countHappiness;
-    cleanPg.value = countClean;
-    } else if(countClean > 60 && countClean <= 100) {
-        countClean = 100
+    
+    document.getElementById('washBtn').onclick = function() {
+        if(countClean <= 60 && countClean > 0){ 
+        countClean += 40;
         countHappiness -= 20;
-
+    
         happinessPg.value = countHappiness;
         cleanPg.value = countClean;
+        } else if(countClean > 60 && countClean <= 100) {
+            countClean = 100
+            countHappiness -= 20;
+    
+            happinessPg.value = countHappiness;
+            cleanPg.value = countClean;
+        }
     }
-}
-
-document.getElementById('runBtn').onclick = function() {
-    if(countHappiness <= 85 && countHappiness > 0){ 
-
-    countHappiness += 15;
-    countFood -= 10;
-
-    happinessPg.value = countHappiness;
-    foodPg.value = countFood;
-    } else if(countHappiness > 85 && countHappiness <= 100) {
-        countHappiness = 100
-        countFood -= 20;
-
+    
+    document.getElementById('runBtn').onclick = function() {
+        if(countHappiness <= 85 && countHappiness > 0){ 
+    
+        countHappiness += 15;
+        countFood -= 10;
+    
         happinessPg.value = countHappiness;
         foodPg.value = countFood;
+        } else if(countHappiness > 85 && countHappiness <= 100) {
+            countHappiness = 100
+            countFood -= 20;
+    
+            happinessPg.value = countHappiness;
+            foodPg.value = countFood;
+        }
     }
 }
+
+
 
 function easyDiff() {
     countFood -= 3;
@@ -113,14 +120,14 @@ function duringLife() {
         alert('You have been lived for ' + countSec + ' seconds')
         isGameScore = false;
     }
-}}
+    }
+}
 function gameOver(){
     if(isGameOver){
     if(foodPg.value <= 0 || cleanPg.value <= 0 || happinessPg.value <= 0) {
         document.querySelector('.actions').hidden = true;
+        document.querySelector('.start').hidden = false;
         isGameOver = false;
-        clearInterval(hardDiff, 100);
-        clearInterval(duringLife, 100);
     }
-}
+    }
 }
